584a3cb128c32c4364a3dadfdd14c37b12c38e09
jackhhh <minjie.h@outlook.com>
2018-06-01 17:49:29 +0800

    optimized class active

--- SystemMLWeb/WebPanel/static/js/base.js
+++ SystemMLWeb/WebPanel/static/js/base.js
@@ -4,8 +4,14 @@
     var set_algorithm_menu_highlight = function(algorithm) {
         var sidebar = $("#sidebar-menu");
         var item = sidebar.find("." + algorithm);
+        var activeItems = document.getElementsByClassName('active');
+        for(var i = 0; i < activeItems.length; i++){
+            activeItems[i].classList.remove("menu-open");
+            activeItems[i].classList.remove("active");
+        }
         if(item.length > 0) {
             item = item[0];
+            $(item).addClass("active");
             var parent = item.parentElement;
             while(parent && !$(parent).hasClass("treeview")) {
                 if(parent.tagName.toLowerCase() === "ul") {
@@ -14,6 +20,7 @@
                 parent = parent.parentElement;
             }
             $(parent).addClass("menu-open");
+            $(parent).addClass("active");
         }
     };
 
